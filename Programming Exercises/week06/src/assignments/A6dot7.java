c05ae863f6ab6cfd721ae3df76b269e8cd9a8627
Kent Anderson <kcanderson.kent@gmail.com>
2019-02-09 10:20:16 -0700

    Week 6 assignments

---
+++ Programming Exercises/week06/src/assignments/A6dot7.java
@@ -0,0 +1,51 @@
+package assignments;
+
+import java.util.Scanner;
+
+public class A6dot7 {
+
+    public static double futureInvestmentValue(
+                            double investmentAmount,
+                            double monthlyInterestRate,
+                            int years ) {
+        
+        return investmentAmount * Math.pow(1 + monthlyInterestRate, years*12);
+    }
+
+
+
+
+
+    public static void main(String[] argv) {
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("The amount invested: ");
+            double amountInvested = scanner.nextDouble();
+
+            System.out.print("Annual interest rate (eg, 10% = 0.10): ");
+            double yearlyInterest = scanner.nextDouble();
+            if (yearlyInterest < 0) {
+                System.err.println("Invalid interest rate");
+                System.exit(1);
+            }
+
+            String tableHeader   = "Years     Future Value\n";
+            String tableTemplate = "%-5d     %12.02f\n";
+
+            System.out.print(tableHeader);
+
+            double futureValue = 0;
+            for (int year = 1; year <= 30; year++) {
+                futureValue = futureInvestmentValue(amountInvested, yearlyInterest/12, 1);
+
+                System.out.printf(tableTemplate, year, futureValue);
+
+                // Set up for the next year
+                amountInvested = futureValue;
+            }
+
+        }
+
+    }
+    
+}
